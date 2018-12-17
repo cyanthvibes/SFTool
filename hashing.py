8f32709dc44da9b7046e50230b9cbba3527b7fb1
JustinMoser99 <45872737+JustinMoser99@users.noreply.github.com>
2018-12-17 14:58:13 +0100

    Add files via upload
    Deze klasse kan SHA1, SHA256 en SHA512 hashwaardes berekenen van een opgegeven pad en alle onderliggende bestanden. Aan de layout van de output moet nog gewerkt worden. Het is misschien handig om dit naar een .csv-bestand te schrijven.

---
+++ hashing.py
@@ -0,0 +1,32 @@
+from py_essentials import hashing as hs
+from pprint import pprint
+import os
+
+
+def hashingSHA1():
+    path = os.path.join("C:") # Pad is de C: schijf
+    hashtree = hs.createHashtree(path, 'sha1') # Van de directoy in 'path' wordt van alle onderliggende bestanden een sha1 hash berekend
+    return hashtree
+
+def hashingSHA256():
+    path = os.path.join("C:")  # Pad is de C: schijf
+    hashtree = hs.createHashtree(path, 'sha256') # Van de directoy in 'path' wordt van alle onderliggende bestanden een sha256 hash berekend
+    return hashtree
+
+def hashingSHA512():
+    path = os.path.join("C:")  # Pad is de C: schijf
+    hashtree = hs.createHashtree(path, 'sha512') # Van de directoy in 'path' wordt van alle onderliggende bestanden een sha512 hash berekend
+    return hashtree
+
+# De main klasse, pprint zorgt voor een 'Pretty-Print'
+def main():
+    print("De SHA1 hashwaardes zijn:")
+    pprint(hashingSHA1())
+    print("De SHA256 hashwaardes zijn:")
+    pprint(hashingSHA256())
+    print("De SHA512 hashwaardes zijn:")
+    pprint(hashingSHA512())
+
+# Voert de main functie uit
+if __name__ == '__main__':
+    main()
