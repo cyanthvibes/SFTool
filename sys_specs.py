cc75116fad03bd52a2874f0f2801ccce7029800e
mariska14 <45872742+mariska14@users.noreply.github.com>
2019-01-04 11:09:59 +0100

    Update sys_specs.py

--- sys_specs.py
+++ sys_specs.py
@@ -4,26 +4,24 @@ Summary: Write system specifications (serial number, disk name and file format) 
 """
 
 import wmi  # pip install wmi
-import psutil   # pip install psutil
-from SFT.system import System
-from SFT.database_helper import insert_data_system_specifications
+from SFTool.system import System
+from SFTool.database_helper import insert_data_system_specifications
 
 
 def register_system_specs_to_database():
-    for part in psutil.disk_partitions():
-        disk_name = part.device
-        file_fomat = part.fstype
 
-        c = wmi.WMI()
-        for pm in c.Win32_PhysicalMedia():
-            serial_number = pm.SerialNumber
+    c = wmi.WMI()
+    for pm in c.Win32_Volume():
+        disk_name = str(pm.wmi_property('DriveLetter').value)
+        serial_number = str(pm.wmi_property('SerialNumber').value)
+        file_fomat = str(pm.wmi_property('FileSystem').value)
 
-            system_specs = System(serial_number, disk_name, file_fomat)
-            print("System specifications: ")
-            print("serial_number: " + pm.SerialNumber)
-            print("disk_name: " + system_specs.disk_name)
-            print("file_fomat: " + system_specs.file_fomat)
-            insert_data_system_specifications(system_specs)
+        system_specs = System(serial_number, disk_name, file_fomat)
+        print("disk_name: " + system_specs.disk_name)
+        print("file_fomat: " + system_specs.file_fomat)
+        print("serial_number: " + system_specs.serial_number)
+
+        insert_data_system_specifications(system_specs)
 
 
 def main():
