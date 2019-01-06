8ef75a639ffa5b0be4de34dcdc8c1760b81374cc
mariska14 <45872742+mariska14@users.noreply.github.com>
2019-01-06 17:16:22 +0100

    Update sys_specs.py

--- sys_specs.py
+++ sys_specs.py
@@ -7,6 +7,7 @@ import wmi  # pip install wmi
 from SFTool.database_helper import insert_data_system_specifications
 
 
+# The class System contains system specifications: serial number, disk name and file fomat
 class System:
     def __init__(self, serial_number, disk_name, file_fomat):
         self.serial_number = serial_number
@@ -23,17 +24,20 @@ class System:
         return self.file_fomat
 
 
+# Writes system/volume specifications to the database
 def register_system_specs_to_database():
+    # Get the system specifications of every volume on the system
     for volume in wmi.WMI().Win32_LogicalDisk():
         disk_name = str(volume.Caption)
         serial_number = str(volume.VolumeSerialNumber)
         file_fomat = str(volume.FileSystem)
 
-        system_specs = System(serial_number, disk_name, file_fomat)     # creates a object system_specs
+        system_specs = System(serial_number, disk_name, file_fomat)   # Creates a object system_specs
+        # Print the system specifications on the console
         print("disk_name: " + system_specs.disk_name)
         print("file_fomat: " + system_specs.file_fomat)
         print("serial_number: " + system_specs.serial_number)
-        insert_data_system_specifications(system_specs)     # write the system specifications to the database
+        insert_data_system_specifications(system_specs)   # Write the system specifications to the database
         print("\n")
 
 
@@ -43,3 +47,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
