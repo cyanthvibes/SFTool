76c34aa510f6f41cb79e670be9b8934826b3f3bc
mariska14 <45872742+mariska14@users.noreply.github.com>
2019-01-06 17:00:56 +0100

    Update database_helper.py

--- database_helper.py
+++ database_helper.py
@@ -1,14 +1,17 @@
 """
 Author: Mariska Temming, S1106242
-Summary: The database_helper initialize the database (three tables) and execute queries (insert data into the database)
+Summary: - The database_helper gets a connection to the database SFT.db
+         - The database_helper initialize the database (SFT.db); it creates three tables: malware_detection,
+           system_specifications and case_information
+         - The database_helper writes data to the database with the insert query
+         - The database_helper selects the data of the database with the select query
 """
 
 import sqlite3
 
 
+# Creates a database connection to the SQLite database specified by db_file
 def get_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file """
     try:
         conn = sqlite3.connect(db_file)  # param db_file: database file
         print("Opened database successfully!")
@@ -19,26 +22,28 @@ def get_connection(db_file):
     return None
 
 
+# Initialize the database
 def initialize_database():
-    conn = get_connection("SFT.db")
-    c = conn.cursor()
+    conn = get_connection("SFT.db")  # Get a connection with the database
+    c = conn.cursor()  # Create a cursor object to call its execute() method to perform SQL commands
 
-    # create four tables into database
+    # Creates three tables into the database
     c.execute("CREATE TABLE IF NOT EXISTS malware_detection(name TEXT, hash REAL, path TEXT, time_detection TEXT)")
     c.execute("CREATE TABLE IF NOT EXISTS system_specifications(serial_number TEXT, disk_name TEXT, file_fomat TEXT)")
     c.execute("CREATE TABLE IF NOT EXISTS case_information(case_name TEXT, start_number REAL, investigator_name TEXT, "
               "comment TEXT, time TEXT)")
 
-    conn.commit()  # commit the queries
-    conn.close()  # close the connection with database
+    conn.commit()  # Commit the queries
+    conn.close()  # Close the connection with the database
     print("Database initialized.")
 
     return None
 
 
+# Inserts data into the table: malware_detection
 def insert_data_malware_detection(malware):
-    conn = get_connection("SFT.db")
-    c = conn.cursor()
+    conn = get_connection("SFT.db")  # Get a connection with the database
+    c = conn.cursor()  # Create a cursor object to call its execute() method to perform SQL commands
 
     c.execute("INSERT INTO malware_detection(name, hash, path, time_detection) "
               "VALUES(?, ?, ?, ?)", (malware.get_name(), malware.get_hash(), malware.get_path(), malware.get_time()))
@@ -46,33 +51,35 @@ def insert_data_malware_detection(malware):
     conn.close()
 
 
+# Inserts data into the table: system_specifications
 def insert_data_system_specifications(system_specs):
-    conn = get_connection("SFT.db")
-    c = conn.cursor()
+    conn = get_connection("SFT.db")  # Get a connection with the database
+    c = conn.cursor()  # Create a cursor object to call its execute() method to perform SQL commands
 
     c.execute("INSERT INTO system_specifications(serial_number, disk_name, file_fomat) "
               "VALUES(?, ?, ?)", (system_specs.get_serial_number(), system_specs.get_disk_name(),
                                   system_specs.get_file_fomat()))
+    conn.commit()  # Commit the queries
+    conn.close()  # Close the connection with the database
 
-    conn.commit()
-    conn.close()
 
-
+# Inserts data into the table: case_information
 def insert_data_case_information(case_data):
-    conn = get_connection("SFT.db")
-    c = conn.cursor()
+    conn = get_connection("SFT.db")  # Get a connection with the database
+    c = conn.cursor()  # Create a cursor object to call its execute() method to perform SQL commands
 
     c.execute("INSERT INTO case_information(case_name, start_number, investigator_name, comment, time) "
               "VALUES(?, ?, ?, ?, ?)", (case_data.get_case_name(), case_data.get_start_number(),
                                         case_data.get_investigator_name(), case_data.get_comment(),
                                         case_data.get_time()))
-    conn.commit()
-    conn.close()
+    conn.commit()  # Commit the queries
+    conn.close()  # Close the connection with the database
 
 
+# Selects the data of the database
 def select_database():
-    conn = get_connection("SFT.db")
-    c = conn.cursor()
+    conn = get_connection("SFT.db")  # Get a connection with the database
+    c = conn.cursor()  # Create a cursor object to call its execute() method to perform SQL commands
 
     print("\n" + "Table: case_information")
     for row in c.execute("SELECT * FROM case_information"):
@@ -86,8 +93,8 @@ def select_database():
     for row in c.execute("SELECT * FROM malware_detection"):
         print(row)
 
-    conn.commit()
-    conn.close()
+    conn.commit()  # Commit the queries
+    conn.close()  # Close the connection with the database
 
 
 def main():
@@ -96,3 +103,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
