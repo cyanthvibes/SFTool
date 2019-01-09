691430e885db15f7ca329fcbbb999caade6d35e5
JustinMoser99 <45872737+JustinMoser99@users.noreply.github.com>
2019-01-09 18:22:03 +0100

    Update compare_hashes.py

--- compare_hashes.py
+++ compare_hashes.py
@@ -1,3 +1,4 @@
+"""
 
 Author:  Justin Moser, S1103774
 Summary: - compare_hashes
@@ -9,8 +10,8 @@ Summary: - compare_hashes
 
 # Omschrijving van de functie staat in de summary
 def compare_hashes():
-    for line in open("system_hashes.txt"):
-        if line in open("virusshare_hashes.txt"):
+    for line in open("system_hashes.txt"): # Leest elke regel van het bestand
+        if line in open("virusshare_hashes.txt"): # Als er een hash overeenkomt, doe dan het volgende:
             print(line)
             with open("virusshare_matches.txt", "a") as e:
                 e.write('{}\n'.format(line))
