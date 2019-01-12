47d0186fddd8da48f4a6ab7221f8461101aadd96
mariska14 <45872742+mariska14@users.noreply.github.com>
2019-01-12 22:15:29 +0100

    Update compare_hashes.py

--- compare_hashes.py
+++ compare_hashes.py
@@ -17,6 +17,17 @@ def compare_hashes():
                 e.write('{}\n'.format(line))
     return line
 
+  
+def TEST_compare_hashes():
+
+    with open('system_hashes.txt', 'r') as system_hashes: # Opent 'virusshare_hashes.txt' en 'system_hashes.txt"
+        for system_hash in system_hashes:
+            hash_from_database = select_virusshare_hashes_by_system_hash(system_hash)
+            print(hash_from_database)
+            with open('virusshare_matches.txt', 'a') as e: # Open het 'virusshare_matches.txt'
+                e.write('{}\n'.format(hash_from_database)) # Schrijf de overeenkomende hash naar het bestand
+
+                
 def main():
     compare_hashes()
 
