7bf0e04e2e8bf99e515668a77f2eae32d21eac11
mariska14 <45872742+mariska14@users.noreply.github.com>
2019-01-12 20:10:51 +0100

    Update database_helper.py

--- database_helper.py
+++ database_helper.py
@@ -2,9 +2,10 @@
 Author: Mariska Temming, S1106242
 Summary: - The database_helper gets a connection to the database SFT.db
          - The database_helper initialize the database (SFT.db); it creates three tables: malware_detection,
-           system_specifications and case_information
+           system_specifications, case_information and virusshare_hashes
          - The database_helper writes data to the database with the insert query
          - The database_helper selects the data of the database with the select query
+         - The database_helper drops the tables case_information, system_specifications and malware_detection
 """
 
 import sqlite3
@@ -32,6 +33,7 @@ def initialize_database():
     c.execute("CREATE TABLE IF NOT EXISTS system_specifications(serial_number TEXT, disk_name TEXT, file_fomat TEXT)")
     c.execute("CREATE TABLE IF NOT EXISTS case_information(case_name TEXT, start_number REAL, investigator_name TEXT, "
               "comment TEXT, time TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS virusshare_hashes(hash TEXT)")
 
     conn.commit()  # Commit the queries
     conn.close()  # Close the connection with the database
@@ -47,8 +49,8 @@ def insert_data_malware_detection(malware):
 
     c.execute("INSERT INTO malware_detection(name, hash, path, time_detection) "
               "VALUES(?, ?, ?, ?)", (malware.get_name(), malware.get_hash(), malware.get_path(), malware.get_time()))
-    conn.commit()
-    conn.close()
+    conn.commit()   # Commit the queries
+    conn.close()  # Close the connection with the database
 
 
 # Inserts data into the table: system_specifications
@@ -76,6 +78,31 @@ def insert_data_case_information(case_data):
     conn.close()  # Close the connection with the database
 
 
+# Inserts data into the table: virusshare_hashes
+def insert_data_virusshare_hashes():
+    conn = get_connection("SFT.db")  # Get a connection with the database
+    c = conn.cursor()  # Create a cursor object to call its execute() method to perform SQL commands
+
+    with open('virusshare_hashes.txt') as e:
+        for line in e.readlines():
+            print(line)
+            c.execute("INSERT INTO virusshare_hashes(hash) VALUES(?)", (line,))
+    conn.commit()  # Commit the queries
+    conn.close()  # Close the connection with the database
+
+
+def select_virusshare_hashes():
+    conn = get_connection("SFT.db")  # Get a connection with the database
+    c = conn.cursor()  # Create a cursor object to call its execute() method to perform SQL commands
+
+    print("\n" + "Table: virusshare_hashes")
+    for row in c.execute("SELECT * FROM virusshare_hashes"):
+        print(row)
+
+    conn.commit()  # Commit the queries
+    conn.close()  # Close the connection with the database
+
+
 # Selects the data of the database
 def select_database():
     conn = get_connection("SFT.db")  # Get a connection with the database
@@ -97,10 +124,30 @@ def select_database():
     conn.close()  # Close the connection with the database
 
 
+# Drops the database tables case_information, system_specifications, malware_detection
+def drop_database():
+    conn = get_connection("SFT.db")  # Get a connection with the database
+    c = conn.cursor()  # Create a cursor object to call its execute() method to perform SQL commands
+
+    print("\n" + "Drop table: case_information")
+    c.execute("DROP TABLE case_information")
+
+    print("\n" + "Drop table: system_specifications")
+    c.execute("DROP TABLE system_specifications")
+
+    print("\n" + "Drop table: malware_detection")
+    c.execute("DROP TABLE malware_detection")
+
+    conn.commit()  # Commit the queries
+    conn.close()  # Close the connection with the database
+
+
 def main():
     initialize_database()
+    # insert_data_virusshare_hashes()
 
 
 if __name__ == '__main__':
     main()
+
 
