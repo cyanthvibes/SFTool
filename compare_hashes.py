f02d7d576ff763c434ab7c8e986af73f0d237d96
mariska14 <45872742+mariska14@users.noreply.github.com>
2019-01-13 18:56:32 +0100

    Update compare_hashes.py

--- compare_hashes.py
+++ compare_hashes.py
@@ -1,33 +1,23 @@
 """
-
 Author:  Justin Moser, S1103774
 Summary: - compare_hashes
                 Compares all the virusshare hashes with the system hashes
                 Writes the corresponding hashes to a file named "virusshare_matches.txt"
                 The "virusshare_matches" file will be opened in hashing.py
-
 """
+
+from SFTool.database_helper import select_virusshare_hashes_by_system_hash
 
 # Omschrijving van de functie staat in de summary
 def compare_hashes():
-    for line in open('virusshare_hashes.txt'): # Leest elke regel van het bestand
-        if line in open("system_hashes.txt"): # Als er een hash overeenkomt, doe dan het volgende:
-            print(line.strip('\n'))
-            with open("virusshare_matches.txt", "a") as e:
-                e.write('{}\n'.format(line))
-    return line
-
-  
-def TEST_compare_hashes():
-
-    with open('system_hashes.txt', 'r') as system_hashes: # Opent 'virusshare_hashes.txt' en 'system_hashes.txt"
-        for system_hash in system_hashes:
+    with open('system_hashes.txt', 'r') as hashes: # Opent 'system_hashes.txt'
+        for system_hash in hashes:
+            system_hash = system_hash.strip()
             hash_from_database = select_virusshare_hashes_by_system_hash(system_hash)
-            print(hash_from_database)
+            print("Virusshare match: " + str(hash_from_database))
             with open('virusshare_matches.txt', 'a') as e: # Open het 'virusshare_matches.txt'
                 e.write('{}\n'.format(hash_from_database)) # Schrijf de overeenkomende hash naar het bestand
 
-                
 def main():
     compare_hashes()
 
