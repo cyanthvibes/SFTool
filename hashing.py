046aa1ede0590a2e60f88d21836ac2653d571092
JustinMoser99 <45872737+JustinMoser99@users.noreply.github.com>
2019-01-13 23:03:31 +0100

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -3,6 +3,7 @@
 Author:  Justin Moser, S1103774
 
 Summary: - get_pathname_and_hashes function
+                Getting user input from 'start_menu.py'
                 Making MD5 hashes of all the files on a system
                 Storing file paths and MD5 hashes in a CSV-file
                 Storing MD5 hashes in a TXT-file
@@ -16,15 +17,15 @@ Summary: - get_pathname_and_hashes function
 
 import hashlib, os, sys, csv
 import win32api # pip install pypiwin32
+import time
 
 drives = win32api.GetLogicalDriveStrings()  # Berekend het aantal schijven
 drives = drives = drives.split('\000')[:-1]
 
 # Omschrijving van de functie staat in de summary
-def get_pathname_and_hashes(): #
+def get_pathname_and_hashes(file_size):
 
-    filesize = int(input("Wat is de grootte (MB's) van de bestanden die u wilt hashen? "))  # Vraagt aan de gebruiker wat de bestandsgrootte moet zijn
-    filesize = filesize * 1048576  # Conversie van 1 byte naar 1MB
+    file_size = file_size * 1048576 # Conversie van 1 byte naar 1MB
 
     for drive in drives: # Voor elke schijf in het systeem wordt de loop uitgevoerd
 
@@ -32,7 +33,7 @@ def get_pathname_and_hashes(): #
             try:
                 for name in files: # Voor elk bestand wordt de loop uitgevoerd
                     filename = (os.path.join(root, name))
-                    if os.path.getsize(filename) <= filesize:
+                    if os.path.getsize(filename) <= file_size:
                         blocksize = 65536
                         path_dict = dict([(hashlib.md5(open(filename, 'rb').read()).hexdigest(), filename)]) # De padnaam en de MD5 hash worden opgeslagen in een dictionary
                         print(path_dict)
@@ -49,14 +50,14 @@ def get_pathname_and_hashes(): #
             except (IOError, PermissionError, MemoryError, FileNotFoundError, UnicodeEncodeError) as x: # Als deze errors voorkomen, dan worden deze bestanden overgeslagen zonder dat het programma stopt
                 print(x)
 
-    return path_dict
+    return file_size
 
 # Omschrijving van de functie staat in de summary
 def convert_md5_to_sha1():
     with open('path_and_hash.csv', 'r') as e: # Het bestand met de padnamen en MD5 hashes wordt geopend
         path_dict = dict(filter(None, csv.reader(e))) # Het CSV-bestand wordt omgezet in een dictionary
 
-    lines = [line.rstrip('\n') for line in open('system_hashes.txt')]  # Virusshare matches worden omgezet in list
+    lines = [line.rstrip('\n') for line in open('virusshare_matches.txt')] # Virusshare matches worden omgezet in list
 
     for key, value in path_dict.items(): # De loop gaat over alle items in de dictionary
         hash = key
@@ -70,7 +71,7 @@ def convert_md5_to_sha1():
 
             with open('malware_sha_path.csv', 'a') as f:
                 writer = csv.writer(f)
-                writer.writerow([checksum, padnaam])    # De SHA! waarde en de padnaam van de malware worden weggeschreven naar het bestand
+                writer.writerow([checksum, padnaam]) # De SHA1 waarde en de padnaam van de malware worden weggeschreven naar het bestand
 
     return checksum
 
