6c6eb8f6f577172523f70276c1a5bb719f4e7d77
mariska14 <45872742+mariska14@users.noreply.github.com>
2019-01-14 21:47:16 +0100

    Update start_menu.py

--- start_menu.py
+++ start_menu.py
@@ -4,7 +4,6 @@ Summary: - The startmenu is the main of the SFTool
          - First it makes a memory dump of the system
          - Then it shows the GUI of the SFTool
          - The user could choose three options in the GUI: "View database", "Quit" and "Start malware scan"
-         - The user can also set an limit to the size of the files that are going to be hashes
          - When the user clicks on "Start malware scan", SFTool is scanning the system of availability of malware
          - When the user clicks on "View database", the database (SFT.db) is shown in the console
          - When the user clicks on "Quit", the GUI closes
@@ -14,27 +13,35 @@ import PySimpleGUI as sg  # pip install PySimpleGUI
 import datetime
 from time import sleep
 
-from case import Case
-from database_helper import insert_data_case_information
-from database_helper import select_database
-from sys_specs import register_system_specs_to_database
-from hashing import get_pathname_and_hashes
-from compare_hashes import compare_hashes
-from hashing import convert_md5_to_sha1
-from network_checker import internet_on
-from virustotal import register_malware_to_database
-from malware_copy import malware_copy
+from SFTool.case import Case
+from SFTool.database_helper import insert_data_case_information
+from SFTool.database_helper import select_database
+from SFTool.sys_specs import register_system_specs_to_database
+from SFTool.hashing import get_pathname_and_hashes
+from SFTool.compare_hashes import compare_hashes
+from SFTool.hashing import convert_md5_to_sha1
+from SFTool.network_checker import internet_on
+from SFTool.virustotal import register_malware_to_database
+from SFTool.malware_copy import malware_copy
+
 
 # Shows the data of the database in the console
 def view_database():
     select_database()
     return None
 
+
 # Update the status mode in the GUI
 def update_status_mode(window, status_mode):
     window.FindElement('_STATUS_').Update(status_mode)
     window.Refresh()
     sleep(1)
+
+
+def creating_memory_dump(window):
+    print("Creating memory dump...")
+    update_status_mode(window, "Creating memory dump...")
+
 
 # The function scan malware is the main program of the SFTool: SFTool is scanning the system of availability of malware
 def scan_malware(window, file_size):
@@ -87,12 +94,12 @@ def scan_malware(window, file_size):
         print('The malware scan is finished!')
         update_status_mode(window, "The malware scan is finished!")
 
-
     except Exception as e:
         print(e)
         result = e
 
     return result
+
 
 # Shows the GUI of the SFTool
 def show_window():
@@ -101,21 +108,27 @@ def show_window():
     # create buttons
     start_malware = sg.Button('Start malware scan', size=(17, 1), font=('Arial', 18), button_color=('black', 'white'),
                               enable_events=True, )
+    create_memory_dump = sg.Button('Create memory dump', size=(17, 1), font=('Arial', 18), button_color=('black', 'white'),
+                              enable_events=True, )
     view_databasee = sg.Button('View Database', size=(13, 1), font=('Arial', 18), button_color=('black', 'white'))
     quit_startmenu = sg.Button('Quit', size=(5, 1), font=('Arial', 18), button_color=('black', 'white'))
 
     # Layout the design of the GUI
     layout = [
         [sg.Text('SFTool - Synergy Forensics Triage Tool', size=(31, 2), text_color='blue', font=('Arial', 30))],
+        [sg.Text('Case information', size=(15, 1), font=('Arial', 16, 'bold'))],
         [sg.Text('Case Name:', size=(15, 1), font=('Arial', 14)), sg.InputText(key='_CASE_NAME_', font=('Arial', 14))],
         [sg.Text('Start Number:', size=(15, 1), font=('Arial', 14)), sg.InputText(key='_START_NUMBER', font=('Arial', 14))],
         [sg.Text('Investigator:', size=(15, 1), font=('Arial', 14)), sg.InputText(key='_INVESTIGATOR_', font=('Arial', 14))],
         [sg.Text('Comment:', size=(15, 1), font=('Arial', 14)), sg.InputText(key='_COMMENT_', font=('Arial', 14))],
+        [empty_row],
+        [sg.Text('Filters', size=(15, 1), font=('Arial', 16, 'bold'))],
         [sg.Text('File Size Limit (MB):', size=(15,1), font=('Arial', 14)), sg.InputText(key='_FILE_SIZE_', font=('Arial', 14))],
+        [empty_row],
         [sg.Text('Status: ', size=(15, 1), font=('Arial', 14)), status_mode],
         [empty_row],
         [empty_row],
-        [view_databasee, empty_row, quit_startmenu, empty_row, start_malware],
+        [view_databasee, empty_row, quit_startmenu, empty_row, create_memory_dump, start_malware],
         [empty_row]
     ]
 
@@ -127,22 +140,31 @@ def show_window():
         if event == 'View Database':
             view_database()
 
+        elif event == 'Create memory dump':
+            creating_memory_dump(window)
+
         elif event == 'Start malware scan':
 
             case_name = value['_CASE_NAME_']
             start_number = value['_START_NUMBER']
             investigator_name = value['_INVESTIGATOR_']
             comment = value['_COMMENT_']
-            file_size = int(value['_FILE_SIZE_'])
+            file_size = value['_FILE_SIZE_']
             time = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
             # If case information is filled then start the malware scan
-            # Else do not start the malware scan and fill in the blanks
             if case_name == '' or start_number == '' or investigator_name == '':
                 sg.Popup("Fill in the case data on the start menu. " + "\n" +
                          "Required: case name, start number and investigator's name" + "\n")
                 print("Fill in the case data on the start menu. " + "\n" +
                       "Required: case name, start number and investigator's name" + "\n")
+
+            # Else if file size is not a number, then show a pop up
+            elif not file_size.isdigit():
+                sg.Popup("Please fill in a number for the file size(MB)")
+                print("Please fill in a number for the file size(MB)")
+
+            # Else do not start the malware scan and fill in the blanks
             else:
                 print("Time: " + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
                 print("Event: " + event + "\n" + "\n", "Case Name: " + "\t" + case_name + "\n",
@@ -163,17 +185,17 @@ def show_window():
 
     return window
 
+
 def sftool():
     try:
-        print("Creates a memory dump of the system...")
-        # memory_dump()
-        # live_triage()
         show_window()
     except Exception as e:
         print(e)
 
+
 def main():
     sftool()
+
 
 if __name__ == '__main__':
     main()
