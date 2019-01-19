403f617fdd4832ec158a51bf649a5a9cb255396d
mariska14 <45872742+mariska14@users.noreply.github.com>
2019-01-19 17:36:21 +0100

    Update network_checker.py

--- network_checker.py
+++ network_checker.py
@@ -9,7 +9,7 @@ from urllib.request import urlopen
 # Checks if there is a connection to the internet
 def internet_on():
     try:
-        urlopen('http://216.58.192.142', timeout=1)  # Test the connection to Google.com
+        urlopen('http://www.google.com', timeout=1)  # Test the connection to Google.com
         return True
     except Exception as e:
         print(e)
