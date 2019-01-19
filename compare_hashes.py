36e6e13e07e99d96e52f6509ce310c39d94352ca
mariska14 <45872742+mariska14@users.noreply.github.com>
2019-01-19 13:50:53 +0100

    Update compare_hashes.py

--- compare_hashes.py
+++ compare_hashes.py
@@ -8,18 +8,23 @@ Summary: - compare_hashes
 
 from SFTool.database_helper import select_virusshare_hashes_by_system_hash
 
+
 # Omschrijving van de functie staat in de summary
 def compare_hashes():
     with open('system_hashes.txt', 'r') as hashes: # Opent 'system_hashes.txt'
         for system_hash in hashes:
             system_hash = system_hash.strip()
             hash_from_database = select_virusshare_hashes_by_system_hash(system_hash)
-            print("Virusshare match: " + str(hash_from_database))
-            with open('virusshare_matches.txt', 'a') as e: # Open het 'virusshare_matches.txt'
-                e.write('{}\n'.format(hash_from_database)) # Schrijf de overeenkomende hash naar het bestand
+            if hash_from_database != '':
+                print("Virusshare match: " + str(hash_from_database))
+                with open('virusshare_matches.txt', 'a') as e: # Open het 'virusshare_matches.txt'
+                    e.write('{}\n'.format(hash_from_database)) # Schrijf de overeenkomende hash naar het bestand
+
 
 def main():
     compare_hashes()
 
+
 if __name__ == '__main__':
     main()
+  
